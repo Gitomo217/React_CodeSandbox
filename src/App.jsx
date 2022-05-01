dba284aafbab9b77d15cc86246d815d3c1e5a3f3
Gitomo217 <doublemoon12@yahoo.co.jp>
2022-05-01 22:35:50 +0900

    jsx記法（序盤）

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// コンポーネント
+const app = () => {
+  return (
+    <React.Fragment>
+      <h1>こんにちは</h1>
+      <p>調子はどう？</p>
+    </React.Fragment>
+  );
+};
+
+export default app;
